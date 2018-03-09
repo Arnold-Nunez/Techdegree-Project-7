60779062edddb9680d6ea2cd53d683babf07a76e
ARNOLD G NUNEZ <31264997+Arnold-Nunez@users.noreply.github.com>
2018-03-09 04:50:26 -0800

    Update app.js

--- Project 7/js/app.js
+++ Project 7/js/app.js
@@ -1,39 +1,64 @@
-// Variables------------------------
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const starter = document.querySelector('.btn__reset');
 let missed = 0;
 
-//Random phrase array
 const phrases = ['one piece', 'attack on titan', 'dragonball', 'black clover', 'naruto'];
 
-//Functions--------------------------
 
 function getRandomPhraseAsArray(arr) {
   //do stuff to any arr that is passed in
-  return arr[Math.round(Math.random() * phrases.length)].split('');
+  return arr[Math.round(Math.random() * arr.length)].split('');
 }
 
 function addPhraseToDisplay(arr) {
   // do stuff any arr that is passed in, and add to `#phrase ul`
   for(let i = 0; i < arr.length; i++) {
     const li = document.createElement('li');
-    const ul = document.querySelector('ul');
+    const ul = document.querySelector('#phrase ul');
     li.textContent = arr[i];
   if (arr[i] != ' ' ) {
-    li.setAttribute('class', 'letter');
+    li.className = 'letter';
   } else {
-    li.setAttribute('class', 'space');
+    li.className = 'space';
   }
   ul.appendChild(li);
  }
 }
 
+starter.addEventListener('click', () => {
+  const overlay = document.getElementById('overlay');
+    if (starter.textContent === 'Start Game') {
+    overlay.style.display = 'none';
+  } else {
+    restart();
+  }
+  });
+
+  getRandomPhraseAsArray(phrases);
+
+  const phraseArray = getRandomPhraseAsArray(phrases);
+  addPhraseToDisplay(phraseArray);
+
+qwerty.addEventLister('click', (e) => {
+  const buttonKey = e.target;
+  if(e.target.tagName === 'button') {
+    buttonKey.className = 'chosen';
+    buttonKey.disabled = true;
+  }
+  const letterFound = checkLetter(buttonKey);
+
+  if (letterFound == null) {
+    missed += 1;
+  }
+  checkWin();
+});
+
 function checkLetter(button) {
   const letter = document.querySelectorAll('.letter');
   for(let i = 0; i < letter.length; i++) {
     if(button.textContent == letter[i].textContent) {
-      letter[i].setAttribute('class', 'show');
+      letter[i].className = 'show';
     } else {
       return null;
     }
@@ -57,39 +82,3 @@ function checkWin() {
 function restart() {
   Location.reload();
 }
-
-//Event Listener----------------------------------
-
-// remove overlay
-starter.addEventListener('click', () => {
-  const overlay = document.getElementById('overlay');
-    if (starter.textContent === 'Start Game') {
-    overlay.style.display = 'none';
-  } else {
-    restart();
-  }
-  });
-
-//keyboard chose
-qwerty.addEventLister('click', (e) => {
-  const buttonKey = e.target;
-  if(e.target.tagName === 'button') {
-    buttonKey.className === 'chosen';
-    buttonKey.disabled = true;
-  }
-  const letterFound = checkLetter(buttonKey);
-
-  if (letterFound == null) {
-    missed =+ 1;
-  }
-  checkWin();
-});
-
-
-//Call in Functions---------------------
-
-
-getRandomPhraseAsArray(phrases);
-
-const phraseArray = getRandomPhraseAsArray(phrases);
-addPhraseToDisplay(phraseArray);
